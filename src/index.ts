6d72b0752c2262dd55e751fc845bdcf4b6507588
Alla Manshina <moralobsession@gmail.com>
2024-02-12 22:33:08 +0400

    feat: 500 error handling

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import http from "node:http";
 
 import { PORT } from "./constants";
-import { get404Response } from "./responseErrors/errors.controller";
+import {
+  get404Response,
+  get500Response,
+} from "./responseErrors/errors.controller";
 import { getUserById, getUsers } from "./users/users.controller";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/api/users" && req.method == "GET") {
-    getUsers(req, res);
-  } else if (
-    req.url?.match(/\/api\/users\/([0-9a-zA-Z\-]+)/) &&
-    req.method == "GET"
-  ) {
-    getUserById(req, res);
-  } else {
-    get404Response(res);
+  try {
+    if (req.url === "/api/users" && req.method == "GET") {
+      getUsers(req, res);
+    } else if (
+      req.url?.match(/\/api\/users\/([0-9a-zA-Z\-]+)/) &&
+      req.method == "GET"
+    ) {
+      getUserById(req, res);
+    } else {
+      get404Response(res);
+    }
+  } catch (err) {
+    get500Response(res);
   }
 });
 
--- src/responseErrors/errors.controller.ts
+++ src/responseErrors/errors.controller.ts
@@ -15,3 +15,11 @@ export const get400Response: ResponseErrorType = (
   res.writeHead(400, { "Content-Type": "application/json" });
   res.end(JSON.stringify({ message }));
 };
+
+export const get500Response: ResponseErrorType = (
+  res,
+  message = "Internal Server Error."
+) => {
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message }));
+};
